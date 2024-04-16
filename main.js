ea20a1a10d54e0b131af2e7a122b1e1d15548869
Ben Ross <benjaminjackross@gmail.com>
2024-04-16 14:47:32 +0100

    Re-enable check for seeing whether Frame backend is up and running
    - Re-enabled healthCheck Single Widget Manager option - is enabled by default
    - Added a fake endpoint named 'health' in the public folder of the locally running single-widget-react repo, which contains the expected JSON data that we get from https://sandbox.frame.hub-box.com/health. File is called: public/health (no file extension). See branch single-widget-react branch 'benr/learning-about-single-widget' to find this change.

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ const singleWidgetManager = new SingleWidgetManager({
   // Enable debugging so that we can see errors getting logged to the Chrome dev tools console. We need this option to see the error that was being thrown when the healthCheck option was enabled by default.
   isDebug: true,
   // We disable the health check. Is enabled by default. When enabled, it will hit an endpoint to check if the Frame back-end is running, and if so, return JSON in the response to tell us whether the back-end service is up or down (running or not). Vite by default expects to be given an HTML document as a response, not JSON as a response, so will produce an error (as shown in the Chrome dev tools console when healthCheck option is enabled). Disabling this option stops this error from happening. If the error happens, we won't see events being logged to the console.
-  healthCheck: false,
+  // To enable and fix the health check, I have added a fake endpoint named 'health' in the public folder of the locally running single-widget-react repo, which contains the expected JSON data that we get from https://sandbox.frame.hub-box.com/health. File is called: public/health (no file extension)
+  // healthCheck: false,
   // This is needed when pointing to Frame (e.g. the sandbox version of Frame URL), but not when pointing to the single-widget-react app (we hardcode the config into the react app's index.html page instead of asking Frame to give us an HTML doc with config data included in the script tag). So we can comment this out when running against a locally running version of the single-widget-react app.
   // iframeParams: {
   //   configId: "macys",
